ddea1f79c346359e947b60d57a903c4ec7158007
Mojahidul Islam <bdemon245@gmail.com>
2024-01-30 23:18:32 +0600

    minor fix

--- resources/views/dashboard/entry/index.blade.php
+++ resources/views/dashboard/entry/index.blade.php
@@ -70,7 +70,7 @@
                 <input @change="$el.value = $el.value.trim().toLowerCase();" value="{{ request()->query('query') }}"
                     autofocus type="search" id="default-search" name="query"
                     class="col-span-3 block w-full p-[0.6rem] ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Search Application ID...">
+                    placeholder="Search Application ID or Police Station...">
                 <div class="col-span-2">
                     <select id="countries" name="doc_type"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
